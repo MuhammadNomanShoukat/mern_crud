6ce4b9d65d4eb66c270787008e02282c309cc22c
LIVEADMINS\noman.shoukat <noman.shoukat@livegreeter.com>
2026-02-26 10:00:37 +0500

    user registeration process and login with JWT implementation

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,18 +1,67 @@
+const userModel = require("../models/user-model");
+
 const home = async (req, res) => {
-    try{
-        res.status(200).json({msg: "controller home route working"})
-    }catch(err){
-        res.status(400).json({msg: "Page not found"})
-    }
-}
+  try {
+    return res.status(200).json({ msg: "controller home route working" });
+  } catch (err) {
+    return res.status(400).json({ msg: "Page not found" });
+  }
+};
 
 const register = async (req, res) => {
-    try{
-        console.log(req.body)
-        res.status(200).json({msg: "controller register route working"})
-    }catch(err){
-        res.status(400).send({msg: "Page not found"})
-    }
-}
+  try {
+    const { username, password, email, phone, isAdmin } = req.body;
 
-module.exports = {home, register}
+    const emailExist = await userModel.findOne({ email });
+    if (emailExist) {
+      return res
+        .status(400)
+        .json({ msg: "Email already exists.", status: false });
+    }
+
+    const userCreated = await userModel.create({
+      username,
+      password,
+      email,
+      phone,
+      isAdmin,
+    });
+
+    return res.status(200).json({
+      msg: "Record saved",
+      token: await userCreated.generateJwtToken(),
+      userId: userCreated._id.toString(),
+      status: true,
+    });
+  } catch (err) {
+    return res.status(500).send({ msg: "server error" });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userExist = await userModel.findOne({ email });
+    if (!userExist) {
+      return res.status(400).json({ msg: "user not exists.", status: false });
+    }
+
+    const userPass = await userExist.comparePassword(password);
+
+    if (userPass) {
+      return res.status(200).json({
+        msg: "Record found",
+        token: await userExist.generateJwtToken(),
+        userId: userExist._id.toString(),
+        status: true,
+      });
+    } else {
+      return res.status(401).json({ msg: "Invailid credentiols" });
+    }
+  } catch (err) {
+    return res.status(500).send({ msg: "server error" });
+  }
+};
+
+module.exports = { home, register, login };
--- models/user-model.js
+++ models/user-model.js
@@ -1,24 +1,69 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: Boolean,
-        default: false
-    }
-})
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const User = new mongoose.model("User", userSchema)
+// Encrypte user entered password
+userSchema.pre("save", async function () {
+  const userData = this;
 
-module.exports = User
+  if (!userData.isModified("password")) {
+    next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(userData.password, salt);
+    userData.password = hashPassword;
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// Generate jwt token
+userSchema.methods.generateJwtToken = async function () {
+  try {
+    return jwt.sign(
+      {
+        userId: this._id.toString(),
+        email: this.email,
+        isAdmin: this.isAdmin,
+      },
+      process.env.JWT_PRIVATE_KEY,
+      { expiresIn: "30d" },
+    );
+  } catch (err) {
+    console.err(err);
+  }
+};
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
